28a196dec95433e878fdcb1133c4194e20c66426
Jonas Valfridsson <jonas@valfridsson.net>
2019-09-01 14:35:14 +0200

    Fucking templates.

--- pendelum.py
+++ pendelum.py
@@ -11,17 +11,21 @@ def postprocess(x: np.ndarray):
     return np.tanh(x) * 2
 
 
+def relu(x):
+    return np.max(x, 0)
+
 model = Model()
 
-model.add(Layer(512, np.tanh))
+model.add(Layer(256, relu))
+model.add(Layer(256, relu))
 
 env = gym.make("Pendulum-v0")
 
 model.compile(env)
 model.train(blackbox=play,
             env=env,
-            iterations=600,
-            samples_per_update=2,
+            iterations=1000,
+            samples_per_update=5,
             postprocess=postprocess,
             show_after=True,
 	    stop_at=-200)
